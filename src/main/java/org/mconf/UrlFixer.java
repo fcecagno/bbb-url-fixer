8edef9b64e2caa2352e11c291b602c00ecea293a
Felipe Cecagno <fcecagno@gmail.com>
2015-09-15 12:58:01 -0300

    fixed regex used to remove the old checksum field

--- src/main/java/org/mconf/UrlFixer.java
+++ src/main/java/org/mconf/UrlFixer.java
@@ -28,12 +28,8 @@ public class UrlFixer {
 			try {
 				String prefix = s.substring(0, s.indexOf("/api/") + 5);
 				String params = s.substring(s.indexOf("?") + 1);
-				params = params.replaceAll("&checksum=[^&]*", "");
+				params = params.replaceAll("[&]?checksum=[^&]*", "");
 				String method = s.substring(s.indexOf("/api/") + 5, s.indexOf("?"));
-				System.out.println("prefix !" + prefix + "!");
-				System.out.println("params !" + params + "!");
-				System.out.println("method !" + method + "!");
-				System.out.println("signing !" + method + params + salt + "!");
 				String checksum = DigestUtils.sha1Hex(method + params + salt);
 				String output = prefix + method + "?" + params + "&checksum=" + checksum;
 				System.out.println("=> " + output);
